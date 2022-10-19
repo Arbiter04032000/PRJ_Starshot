d07f035895339f938a740684ec8035525370c13c
Xavier Hancock <Arbiter04032000@gmail.com>
2022-10-20 05:55:11 +1100

    Revert "BTW Quickfix for missing/invalid DLL"
    This reverts commit 335a69ddb442db5c2ec863480b146c8706486ca7.

---
+++ Project/Source/PRJ_Starshot/PRJ_Starshot.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PRJ_Starshot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PRJ_Starshot, "PRJ_Starshot" );
